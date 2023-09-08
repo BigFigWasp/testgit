78a0d9e6c5fb81113abe8255d00ddcf0d33fd83c
Chilipapa <firek@web.de>
2023-09-08 12:12:22 +0200

    001 - ulibranch neue test.py, neue funktionen

---
+++ funktionen_uli.py
@@ -0,0 +1,5 @@
+def f1():
+    return 77
+
+def f2():
+    return 88
--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-def uli_fkt():
-    return 77
-
-def uli_fkt2():
-    return "Uli"
-
-def f3():
-    return 33
-
-print("Hello, World!")
-print(uli_fkt())
-print(uli_fkt2())
-print(f3())
-=======
 def jan_fkt():
     return 66
 
 print("Hello, World!")
 print(jan_fkt())
->>>>>>> 1a414e0a8e24f5477d89be81cdd3dede03ac5b4f
